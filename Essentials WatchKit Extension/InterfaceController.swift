3e431e0085e5677e876bdbc61fac53072411ed6b
Jamal Yauhari Martinez <jamalyauharimcollege@gmail.com>
2019-06-15 15:36:59 -0400

    first commit

---
+++ Essentials WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Essentials WatchKit Extension
+//
+//  Created by Jamal Yauhari on 6/15/19.
+//  Copyright © 2019 Jamal Yauhari. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
